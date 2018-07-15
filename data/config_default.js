a71677eceefadbe4a4f0ed592cf8866f4dc8e3ad
Preston Smith <preston.s1993@gmail.com>
2018-07-15 11:15:33 -0500

    Reorder config options

--- data/config_default.js
+++ data/config_default.js
@@ -1,11 +1,9 @@
 exports.RUN = false; // Set to true when you've configured the script
 exports.PORT = 7777; // Server port
+exports.SECRET = ""; // Github webhooks secret
+exports.UPLOAD_BUILD = true; // Upload build to release
 
 exports.USER_NAME = ""; // Your github username
 exports.USER_PASS = ""; // Your github password
 exports.USER_EMAIL = ""; // Your github email
 exports.USER_REALNAME = ""; // Your real name
-
-exports.SECRET = ""; // Github webhooks secret
-
-exports.UPLOAD_BUILD = true; // Upload build to release
