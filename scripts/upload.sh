3a77d631f27699384e35376aa0e125579b02febb
Preston Smith <preston.s1993@gmail.com>
2018-08-04 16:44:55 -0500

    rscplus_webhooks: Handle windows build packaging

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,6 +7,10 @@
 response=$(curl --silent https://api.github.com/repos/$1/$3/releases/tags/Latest)
 release_id=$(echo $response | grep -Po '"id"\s*:\s*\K\d+' | head -1)
 
+# Package windows build
+cp data/windows_wrapper.zip rscplus/dist/rscplus-windows.zip
+zip -u rscplus/dist/rscplus-windows.zip rscplus/dist/rscplus.jar
+
 if [ -e rscplus/dist/rscplus.jar ]; then
   curl --user "$1:$2" \
        -H "Content-Type: application/zip" \
