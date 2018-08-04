34ee54a5c24fde9d43ec23886f19da735d681c53
Preston Smith <preston.s1993@gmail.com>
2018-08-04 17:00:09 -0500

    rscplus_webhooks: Fix windows build packaging

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -8,21 +8,23 @@ response=$(curl --silent https://api.github.com/repos/$1/$3/releases/tags/Latest
 release_id=$(echo $response | grep -Po '"id"\s*:\s*\K\d+' | head -1)
 
 # Package windows build
+rm -rf rscplus/dist/rscplus-windows.zip
 cp data/windows_wrapper.zip rscplus/dist/rscplus-windows.zip
-zip -u rscplus/dist/rscplus-windows.zip rscplus/dist/rscplus.jar
+cd rscplus/dist
+zip -u rscplus-windows.zip rscplus.jar
 
-if [ -e rscplus/dist/rscplus.jar ]; then
+if [ -e rscplus.jar ]; then
   curl --user "$1:$2" \
        -H "Content-Type: application/zip" \
        --request POST \
-       --data-binary "@rscplus/dist/rscplus.jar" \
+       --data-binary "@rscplus.jar" \
        https://uploads.github.com/repos/$1/$3/releases/$release_id/assets?name=rscplus.jar
 fi
 
-if [ -e rscplus/dist/rscplus-windows.zip ]; then
+if [ -e rscplus-windows.zip ]; then
   curl --user "$1:$2" \
        -H "Content-Type: application/zip" \
        --request POST \
-       --data-binary "@rscplus/dist/rscplus-windows.zip" \
+       --data-binary "@rscplus-windows.zip" \
        https://uploads.github.com/repos/$1/$3/releases/$release_id/assets?name=rscplus-windows.zip
 fi
